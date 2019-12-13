816f9481c4d2ef419f97ed3b32a4a66ecbf6955f
Galina <galina@Olegs-MacBook-Pro.local>
2019-12-13 14:39:18 +0300

    Converting functional components to class-components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//         <Greeting />
+//           </div>
+//   )
+// }
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header username="Galina" />
+        <Greeting />
+      </div>
+    );
+  }
+}
+
+// function Header(props) {
+//   return (
+//     <header>
+//       <p>Welcom, {props.username}!</p>
+//     </header>
+//   );
+// }
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <p>Welcom, {this.props.username}</p>
       </header>
-    </div>
-  );
+    );
+  }
+}
+
+// function Greeting() {
+//   const date = new Date();
+//   const hours = date.getHours();
+//   let timeOfDay;
+
+//   if (hours < 12) {
+//     timeOfDay = "morning";
+//   } else if (hours >= 12 && hours < 17) {
+//     timeOfDay = "afternon";
+//   } else {
+//     timeOfDay = "night";
+//   }
+//   return <h1>Good {timeOfDay} to you, sir or madam!</h1>;
+// }
+
+class Greeting extends Component {
+  render() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+
+    if (hours < 12) {
+      timeOfDay = "morning";
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternon";
+    } else {
+      timeOfDay = "night";
+    }
+
+    return <h1>Good {timeOfDay} to you, sir or madam!</h1>;
+  }
 }
 
 export default App;
